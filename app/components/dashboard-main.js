3e3f1975d4f4cdb6eefe1a47d63ab7e32e97503f
monsteronfire <monsteronfire@gmail.com>
2016-09-26 19:55:29 +0800

    Use ember-bootstrap addon * The addon implements bootstrap elements as native Ember components

--- app/components/dashboard-main.js
+++ app/components/dashboard-main.js
@@ -3,16 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     collapse() {
-      this.$('#instructions').collapse('toggle');
-    },
-
-    didInsertElement() {
-      this.$('#toggle').tooltip({title: 'Click here to toggle instructions'});
-    },
-
-    willDestroyElement() {
-      //remember to clean up your room, kids
-      this.$('#toggle').tooltip('destroy');
+      this.toggleProperty('collapsed');
     }
   }
 });
